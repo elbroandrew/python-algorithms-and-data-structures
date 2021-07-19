f5f57f59c811e1fa6f06202271970f7f1aba5722
Andrew <elbroandrew@gmail.com>
2021-07-19 21:02:49 +1000

    bubble sort optimization -- adding no_swaps variable

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,13 +1,19 @@
 def bubble_sort(arr):
     for i in range(len(arr), 0, -1):
+        no_swaps = True
         for j in range(0, i - 1):
             if arr[j] > arr[j + 1]:
                 # SWAP
                 temp = arr[j]
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
+
+                # Check Swaps
+                no_swaps = False
+            if no_swaps:
+                break
     return arr
 
 
-x = bubble_sort([123, -534, 435, 65, 234, 12, 12, -54, 0, 98, 1])
+x = bubble_sort([123, 1, 2, 3, 4, 5, 6, 7, 8, 98])
 print(x)

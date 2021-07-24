03988cc923071c5e40cf7e0fc91570dbf8adf3cb
Andrew <elbroandrew@gmail.com>
2021-07-24 11:29:22 +1000

    используется только соритрованный массив

--- binary_search_trees.py
+++ binary_search_trees.py
@@ -9,3 +9,10 @@ def build_bst(my_list):
 
     print(f"Middle index: {middle_idx}")
     print("Middle value: " + str(middle_value))
+
+
+# только сортированный массив использую
+sorted_list = [12, 13, 14, 15, 16]
+binary_search_tree = build_bst(sorted_list)
+print(binary_search_tree)
+

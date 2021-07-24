9ac7f14de0277263bd8384af4adc5b4957e68c13
Andrew <elbroandrew@gmail.com>
2021-07-24 11:27:40 +1000

    find middle index and its value

--- binary_search_trees.py
+++ binary_search_trees.py
@@ -1,3 +1,11 @@
 def build_bst(my_list):
     if len(my_list) == 0:
         return "No Child"
+
+    # середина списка с округлением вниз
+    middle_idx = len(my_list)//2
+
+    middle_value = my_list[middle_idx]
+
+    print(f"Middle index: {middle_idx}")
+    print("Middle value: " + str(middle_value))

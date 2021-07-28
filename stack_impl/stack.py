f45706d5355acf57e3b7c48d7e889ba7d68fb29a
andoraibu <elbroandrew@gmail.com>
2021-07-28 22:52:25 +1000

    имплиментация стека

---
+++ stack_impl/stack.py
@@ -0,0 +1,26 @@
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        if len(self.items) == 0:
+            return None
+        return self.items[len(self.items)-1]
+    
+    def getSize(self):
+        return len(self.items)
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    
+
+
+
+

ce60e14492e7fae1124e1e2ad7291a867bd94397
andoraibu <elbroandrew@gmail.com>
2021-07-28 22:59:48 +1000

    имплиментация стека

--- stack_impl/app.py
+++ stack_impl/app.py
@@ -1,2 +1,24 @@
 from stack import Stack
 
+cars = Stack()
+
+cars.push('Mazda')
+cars.push('Honda')
+cars.push('Toyota')
+
+print(cars.items)
+
+print(cars.next_to_remove())
+
+print(cars.pop())
+
+print(cars.items)
+
+print(cars.next_to_remove())
+
+print(cars.push('Nissan'))
+
+print(cars.items)
+
+print(cars.next_to_remove())
+

e47e89b197462b9d2c67cc29706ab9a9619637fd
andoraibu <elbroandrew@gmail.com>
2021-07-29 12:20:49 +1000

    another recursive solution for sum_of_digits

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -1,4 +1,5 @@
 
+# recursive solution
 def sum_of_digits(n):
     if n < 10:
         return n
@@ -9,3 +10,13 @@ def sum_of_digits(n):
 
 x = sum_of_digits(125)
 print(x)
+
+# another recursive solution
+def sum_digits(n):
+    reminder = n % 10
+    prime = n // 10
+    # base case
+    if prime == 0:
+        return reminder
+        
+    return reminder + sum_digits(prime)

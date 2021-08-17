cdd114912fca0c68ff0b054c6d65b9a222b58f0f
andoraibu <elbroandrew@gmail.com>
2021-08-17 20:52:09 +1100

    feat: add factorial with test cases

---
+++ factorial.py
@@ -0,0 +1,18 @@
+def factorial(number):
+    result = 1
+    for i in range(1, number+1):
+        result *= i
+
+    return result
+
+
+if __name__ == '__main__':
+    cases = [
+        (0, 1),
+        (4, 24),
+        (5, 120),
+    ]
+
+    for input_number, output in cases:
+        assert factorial(input_number) == output
+

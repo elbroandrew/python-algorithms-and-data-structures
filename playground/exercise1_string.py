1775d406743fa015bce5d7e462b6e552b8e78a1c
andoraibu <elbroandrew@gmail.com>
2021-08-29 20:16:40 +1100

    feat: add exercise_1 for stirng

---
+++ playground/exercise1_string.py
@@ -0,0 +1,22 @@
+# Time to review all the basic data types we learned! This should be a
+# relatively straight-forward and quick assignment.
+
+###############
+## Problem 1 ##
+###############
+
+# Given the string:
+s = 'flask'
+
+# Use indexing to print out the following:
+# 'f'
+
+# 's'
+
+# 'ask'
+
+# 'las'
+
+# 'k'
+
+# Bonus: Use indexing to reverse the string

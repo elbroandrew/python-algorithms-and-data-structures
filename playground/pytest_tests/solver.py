248343d9cb4e3fe8ceb01bc2f26cb980ab06f2b1
andoraibu <elbroandrew@gmail.com>
2021-08-29 20:36:41 +1100

    refactor: add algorithm to solve equation

--- playground/pytest_tests/solver.py
+++ playground/pytest_tests/solver.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 def add(a, b):
     return a + b
 
@@ -10,4 +13,21 @@ def square_equation_solver(a, b, c):
         )
     ):
         raise TypeError("Not valid type for param")
-    print("Types are OK")
+    if a == 0:
+        if b == 0:
+            return None, None
+        return -c / b, None
+    d = b ** 2 - 4 * a * c
+    if d < 0:
+        return None, None
+    d_root = sqrt(d)
+    divider = 2 * a
+    x1 = (-b + d_root)/divider
+    x2 = (-b - d_root)/divider
+    if d == 0:
+        x2 = None
+    elif x2 > x1:
+        x1, x2 = x2, x1
+
+    return x1, x2
+

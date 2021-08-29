ed041a5764da4553fa8e44dfbc61f7328e639a92
andoraibu <elbroandrew@gmail.com>
2021-08-29 19:11:16 +1100

    refactor: change func

--- playground/pytest_tests/solver.py
+++ playground/pytest_tests/solver.py
@@ -2,5 +2,12 @@ def add(a, b):
     return a + b
 
 
-def square_equation_solver(a: int, b: int, c: int) -> int:
-    pass
+def square_equation_solver(a, b: int, c: int) -> int:
+    if not all(
+        map(
+            lambda param: isinstance(param, (int, float)),
+            (a, b, c)
+        )
+    ):
+        raise TypeError("Not valid type for param 'a'")
+    print("Types are OK")

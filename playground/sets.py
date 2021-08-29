b61a4d734c509fa55a93ad709725644881093355
andoraibu <elbroandrew@gmail.com>
2021-08-29 20:15:29 +1100

    feat: add sets

---
+++ playground/sets.py
@@ -0,0 +1,18 @@
+my_set = set()
+my_set.add(1)
+my_set.add(1)
+my_set.add(1)
+my_set.add(1)
+my_set.add(2)
+my_set.add(2)
+my_set.add(2)
+my_set.add(4)
+my_set.add(4)
+my_set.add(4)
+print(my_set)
+'''
+можно передать лист
+'''
+my_list = [1,1,1,1,2,3,4,5,5,5,5,5,5,5]
+another_set = set(my_list)
+print(another_set)

29393b17f87006adb63ae348e73d978325e89fcb
andoraibu <elbroandrew@gmail.com>
2021-09-02 18:42:57 +1100

    feat: add check if result is tuple

--- playground/pytest_tests/test_solver.py
+++ playground/pytest_tests/test_solver.py
@@ -13,3 +13,12 @@ class TestSquareEquationSolver(TestCase):
         with self.assertRaises(TypeError):
             square_equation_solver("", 1, 1.5)
 
+    def test_result_is_tuple(self):
+        res = square_equation_solver(0,0,0)
+        self.assertIsInstance(res, tuple)
+
+    def test_no_results(self):
+        res = square_equation_solver(0,0,1)
+        self.assertEqual(res, (None, None))
+
+

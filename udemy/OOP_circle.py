28f6996e20b3c9499631de97adf5c3a9570ab7ee
Andrew <elbroandrew@gmail.com>
2021-09-29 11:41:56 +1000

    fix: add comments

--- udemy/OOP_circle.py
+++ udemy/OOP_circle.py
@@ -1,5 +1,5 @@
 class Circle:
-    pi: float = 3.14
+    pi: float = 3.14 # т.е. это пи может быт использ как self.pi, а так же как Class.pi
 
     def __init__(self, radius):
         self.radius = radius
@@ -11,3 +11,4 @@ class Circle:
 if __name__ == '__main__':
     c = Circle(20)
     print(c.area())
+    print(Circle.pi)

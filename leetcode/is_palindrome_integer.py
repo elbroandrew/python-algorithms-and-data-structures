6781db25495715a3c9c283628d8c2e843951cdfe
Andrew <elbroandrew@gmail.com>
2021-10-07 09:43:51 +1000

    feat: is palindrome integer solution2 no string

--- leetcode/is_palindrome_integer.py
+++ leetcode/is_palindrome_integer.py
@@ -1,11 +1,25 @@
 class Solution:
-
+    """ -121 is false """
     # trivial solution with string
     @staticmethod
     def isPal_1(x: int) -> bool:
         return str(x) == str(x)[::-1]
 
+    # without string
+    @staticmethod
+    def isPal_2(x: int) -> bool:
+        y = x
+        s = 0
+        while x > 0:
+            s *= 10
+            s += x % 10
+            x //= 10
+        return y == s
+
 
 if __name__ == '__main__':
-    y = Solution.isPal_1(-121)
-    print(y)
+    s1 = Solution.isPal_1(-121)
+    print(s1)
+
+    s2 = Solution.isPal_2(-121)
+    print(s2)

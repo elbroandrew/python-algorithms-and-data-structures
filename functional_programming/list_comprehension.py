ce46a086d8c83d01e98bbee21ee2e3b091ddd84b
Andrew <elbroandrew@gmail.com>
2021-12-19 10:25:01 +1000

    feat: add examples list comprehension

--- functional_programming/list_comprehension.py
+++ functional_programming/list_comprehension.py
@@ -17,7 +17,7 @@ for x in range(5, 10):
         x += 1
 
 print([x * 2 if x % 2 == 0 else x + 1 for x in range(5, 10)])
-    
+
 '''Генератор списков - способ построить новый список, 
 применяя выражение к каждому элементу последовательности. '''
 
@@ -25,3 +25,10 @@ my_list = [2, 5, 1, 4]
 
 result_list = [c * 2 for c in my_list]
 print(result_list)
+
+'''Конвертировать в булевые'''
+print([bool(val) for val in ['', 0, (), 1]])  # false, false, false, true
+
+'''Еще примеры'''
+'''четные умножаем на два, нечетные делим'''
+print([x * 2 if x % 2 == 0 else x / 2 for x in [1, 2, 3, 4, 5, 6, 7, 8]])

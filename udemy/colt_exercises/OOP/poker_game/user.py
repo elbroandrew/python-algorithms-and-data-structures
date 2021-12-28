e6ab51f346b954a0c434e0bfb52e7f6f20acf4f4
Andrew <elbroandrew@gmail.com>
2021-12-28 12:15:48 +1000

    feat: add methods OOP

--- udemy/colt_exercises/OOP/poker_game/user.py
+++ udemy/colt_exercises/OOP/poker_game/user.py
@@ -1,8 +1,16 @@
 class User:
-    def __init__(self, first_name, last_name, age):
+    def __init__(self, first_name: str, last_name: str, age: int):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def initials(self):
+        return f"{self.first_name[0].upper()}.{self.last_name[0].upper()}."
 
 
+
+u = User('john', "Doe", 34)
+print(u.initials())

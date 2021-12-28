10adf2645ceb6a3505e767a34d0f52b7984140bd
Andrew <elbroandrew@gmail.com>
2021-12-28 12:16:53 +1000

    feat: add class attribute

--- udemy/colt_exercises/OOP/poker_game/user.py
+++ udemy/colt_exercises/OOP/poker_game/user.py
@@ -1,8 +1,15 @@
 class User:
+    active_users = 0
+
     def __init__(self, first_name: str, last_name: str, age: int):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+        User.active_users += 1
+
+    def logout(self):
+        User.active_users -= 1
+        print(f"{self.first_name} has logged out.")
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"

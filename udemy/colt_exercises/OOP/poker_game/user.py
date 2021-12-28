90a89dcae7087e5f62d783a3ea2850ffbbb60e37
Andrew <elbroandrew@gmail.com>
2021-12-28 12:48:35 +1000

    feat: add another classmethod to create obj from string

--- udemy/colt_exercises/OOP/poker_game/user.py
+++ udemy/colt_exercises/OOP/poker_game/user.py
@@ -8,9 +8,9 @@ class User:
     @classmethod
     def from_string(cls, data_string: str):
         first, last, age = data_string.split(',')
-        return cls(first, last, age)
+        return cls(first, last, int(age))
 
-    def __init__(self, first_name: str, last_name: str, age):
+    def __init__(self, first_name: str, last_name: str, age: int):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age

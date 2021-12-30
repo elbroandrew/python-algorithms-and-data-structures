3e8f994da86ee1993f084e76fc0cddd49c99dab6
Andrew <elbroandrew@gmail.com>
2021-12-30 13:13:56 +1000

    feat: moderator

--- udemy/colt_exercises/OOP/poker_game/moderator.py
+++ udemy/colt_exercises/OOP/poker_game/moderator.py
@@ -2,9 +2,15 @@ from udemy.colt_exercises.OOP.poker_game.user import User
 
 
 class Moderator(User):
+    total_mods = 0
     def __init__(self, first, last, age, community):
         super().__init__(first, last, age)
         self.community = community
+        Moderator.total_mods += 1
+
+    @classmethod
+    def display_active_mods(cls):
+        return cls.total_mods
 
     def remove_post(self):
         return f"{self.full_name()} removed a post from the {self.community} community."

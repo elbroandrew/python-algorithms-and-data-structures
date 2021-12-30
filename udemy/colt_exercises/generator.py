3f9f8bd88c7477ad6e29e5138d11f213af5a58c7
Andrew <elbroandrew@gmail.com>
2021-12-30 19:45:11 +1000

    feat: generator EXPRESSION

--- udemy/colt_exercises/generator.py
+++ udemy/colt_exercises/generator.py
@@ -4,8 +4,16 @@ def count_up_to_max(max):
         yield count
         count += 1
 
-counter = count_up_to_max(5)
-print(next(counter))
-print(next(counter))
-print(next(counter))
-print(next(counter))
+# counter = count_up_to_max(5)
+# print(next(counter))
+# print(next(counter))
+# print(next(counter))
+# print(next(counter))
+
+
+# GENERATOR EXPRESSION
+g = (num for num in range(1, 10))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))

fc217f9e6d365678bc164ce1299661435e426f22
Andrew <elbroandrew@gmail.com>
2021-12-31 13:42:10 +1000

    feat: insert into DB another way

--- SQLite/friends.py
+++ SQLite/friends.py
@@ -5,8 +5,11 @@ c = conn.cursor()
 
 # execute some sql
 #c.execute("CREATE TABLE friends (first_name TEXT, last_name TEXT, closeness INTEGER);")
-insert_query = '''insert into friends
-                    values ('Marry', 'Lewis', 7)'''
-c.execute(insert_query)
+# insert_query = '''insert into friends
+#                     values ('Marry', 'Lewis', 7)'''
+
+form_first = "Dana"
+query = f"INSERT INTO friends (first_name) VALUES ('{form_first}')"
+c.execute(query)
 conn.commit()
 conn.close()

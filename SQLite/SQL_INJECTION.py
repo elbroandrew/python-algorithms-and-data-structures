23e31195ee6788fae4d4824a8836c17a0a2b9898
Andrew <elbroandrew@gmail.com>
2021-12-31 14:12:38 +1000

    feat: SQL Injection add users to table

--- SQLite/SQL_INJECTION.py
+++ SQLite/SQL_INJECTION.py
@@ -3,6 +3,13 @@ conn = sqlite3.connect("user.db")
 
 cursor = conn.cursor()
 
-cursor.execute("CREATE TABLE users (username TEXT, password TEXT);")
+list_of_users = [
+    ("John", "123"),
+    ("Sarah", "555"),
+    ("Terminator", "800")
+]
+
+cursor.executemany("INSERT INTO users VALUES (?, ?)", list_of_users)
+
 conn.commit()
 conn.close()

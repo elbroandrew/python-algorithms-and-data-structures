7bad6bd0b302a23dd8d5cb7f34c11c108147724f
Andrew <elbroandrew@gmail.com>
2021-12-31 13:39:35 +1000

    feat: insert into DB sqlite3

--- SQLite/friends.py
+++ SQLite/friends.py
@@ -4,6 +4,9 @@ conn = sqlite3.connect("my_friends.db") # create db
 c = conn.cursor()
 
 # execute some sql
-c.execute("CREATE TABLE friends (first_name TEXT, last_name TEXT, closeness INTEGER);")
+#c.execute("CREATE TABLE friends (first_name TEXT, last_name TEXT, closeness INTEGER);")
+insert_query = '''insert into friends
+                    values ('Marry', 'Lewis', 7)'''
+c.execute(insert_query)
 conn.commit()
 conn.close()

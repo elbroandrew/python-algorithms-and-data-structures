da544138b46fe1248287d5778e3453bf752033f8
Andrew <elbroandrew@gmail.com>
2021-12-31 13:45:10 +1000

    feat: insert into DB with TUPLE

--- SQLite/friends.py
+++ SQLite/friends.py
@@ -4,12 +4,18 @@ conn = sqlite3.connect("my_friends.db") # create db
 c = conn.cursor()
 
 # execute some sql
-#c.execute("CREATE TABLE friends (first_name TEXT, last_name TEXT, closeness INTEGER);")
+# c.execute("CREATE TABLE friends (first_name TEXT, last_name TEXT, closeness INTEGER);")
 # insert_query = '''insert into friends
 #                     values ('Marry', 'Lewis', 7)'''
 
-form_first = "Dana"
-query = f"INSERT INTO friends (first_name) VALUES ('{form_first}')"
-c.execute(query)
+# BAD WAY !
+# form_first = "Dana"
+# query = f"INSERT INTO friends (first_name) VALUES ('{form_first}')"
+
+# BETTER WAY
+form_first = "John"
+query = f"INSERT INTO friends (first_name) VALUES (?)"
+
+c.execute(query, (form_first,) ) # add tuple
 conn.commit()
 conn.close()

463c3a003add323c4cb65f108a5c659ba3f4ec44
Andrew <elbroandrew@gmail.com>
2021-12-31 13:58:46 +1000

    feat: select from table

--- SQLite/friends.py
+++ SQLite/friends.py
@@ -2,15 +2,9 @@ import sqlite3
 conn = sqlite3.connect("my_friends.db") # create db
 # create cursor object
 c = conn.cursor()
-
-people = [
-    ("Roland", "Amundsen", 5),
-    ("Rosa", "Perks", 3),
-    ("Neil", "Armstrong", 5),
-    ("Henry", "Hudson", 2)
-]
-
-c.executemany("INSERT INTO friends VALUES (?, ?, ?)", people)
+c.execute("SELECT * FROM friends")
+for person in c:
+    print(person)
 
 conn.commit()
 conn.close()

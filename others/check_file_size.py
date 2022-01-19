3773b17ac80467191f47bb2da40f21af686316bb
Andrew <elbroandrew@gmail.com>
2022-01-19 17:38:29 +1000

    feat: add check file size

---
+++ others/check_file_size.py
@@ -0,0 +1,14 @@
+import os
+
+path = "../tests.py"
+
+f = os.path.getsize(path)
+print(f)
+
+
+# better solution (more quicker)
+def get_file_size(file_path: str):
+    return os.stat(file_path).st_size
+
+
+print(get_file_size(path))

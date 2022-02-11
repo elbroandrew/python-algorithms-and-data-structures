8dd12d0e722a32944b2a95624e7defc3f3ceda9c
Andrew <elbroandrew@gmail.com>
2022-02-11 19:46:40 +1000

    feat: change to itertools zip_longest

--- others/dict_from_lists/start.py
+++ others/dict_from_lists/start.py
@@ -1,10 +1,16 @@
 from typing import List
 import unittest
+from itertools import zip_longest
+
+"""Есть два списка разной длины. В первом содержатся ключи, а во втором значения. 
+Напишите функцию, которая создаёт из этих ключей и значений словарь. 
+Если ключу не хватило значения, в словаре должно быть значение None. 
+Значения, которым не хватило ключей, нужно игнорировать."""
 
 
 def dict_from_keys_and_values(list1: List, list2: List) -> dict:
 
-    return dict(zip(list1, list2)) if (len(list1) <= len(list2)) else dict(map(None, list1, list2))
+    return dict(zip(list1, list2)) if (len(list1) <= len(list2)) else dict(zip_longest(list1, list2))
 
 
 class CreateDict(unittest.TestCase):
@@ -13,6 +19,13 @@ class CreateDict(unittest.TestCase):
         keys = ['one', 'two', 'three']
         values = [1, 2, 3]
         result = dict(one=1, two=2, three=3)
+
+        self.assertEqual(dict_from_keys_and_values(keys, values), result)
+
+    def test_len_keys_gt_values(self):
+        keys = ['one', 'two', 'three', 'four']
+        values = [1, 2, 3]
+        result = dict(one=1, two=2, three=3, four=None)
 
         self.assertEqual(dict_from_keys_and_values(keys, values), result)
 

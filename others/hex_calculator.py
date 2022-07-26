28b78bcf401aee9ab92fa3ceb70985f9465b5299
elbroandrew <elbroandrew@gmail.com>
2022-07-27 08:32:49 +1000

    delete hex calc

--- others/hex_calculator.py
+++ others/hex_calculator.py
@@ -1,22 +1,2 @@
-hex = [i for i in range(0, 10)]
 
-for i in "ABCDEF":
-    hex.append(i)
-
-x = 3331
-base = 16
-res = []
-
-while x > base:
-    y = x // base
-    res.insert(0, str(hex[x - y* base]))
-    x = y
-    
-
-if len(res) != 0:
-    res.insert(0, str(hex[x]))
-    res = ''.join(res)
-    print(res)
-else:
-  print(str(hex[x]))
     

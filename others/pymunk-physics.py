4eb1ff5511b74c58af3b958fda9564aa99800ebe
andrew <elbroandrew@gmail.com>
2022-08-08 22:58:41 +1000

    pymunk 2

--- others/pymunk-physics.py
+++ others/pymunk-physics.py
@@ -8,52 +8,63 @@ pygame.init()
 WIDTH, HEIGHT = 1000, 800
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+
 def draw(space, window, draw_options):
-	window.fill("white")
-	space.debug_draw(draw_options)
-	pygame.display.update()
+    window.fill("white")
+    space.debug_draw(draw_options)
+    pygame.display.update()
+
 
 def create_boundaries(space, width, height):
-	rects = [
-		[(width/2, height-10), (width, 20)]
-	]
+    rects = [
+        [(width / 2, height - 10), (width, 20)],
+        [(width / 2, 10), (width, 20)],
+        [(10, height / 2), (20, height)],
+        [(width -10, height / 2), (20, height)],
+    ]
+    for pos, size in rects:
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        body.position = pos
+        shape = pymunk.Poly.create_box(body, size)
+        space.add(body, shape)
+
 
 def create_ball(space, radius, mass):
-	body = pymunk.Body()
-	body.position = (300 ,300)
-	shape = pymunk.Circle(body, radius)
-	shape.mass = mass
-	shape.color = (255, 100, 100, 100)
-	space.add(body, shape)
-	return shape
+    body = pymunk.Body()
+    body.position = (300, 300)
+    shape = pymunk.Circle(body, radius)
+    shape.mass = mass
+    shape.color = (255, 100, 100, 100)
+    space.add(body, shape)
+    return shape
+
 
 def run(window, width, height):
-	run = True
-	clock = pygame.time.Clock()
-	fps = 60
-	dt = 1/fps
+    run = True
+    clock = pygame.time.Clock()
+    fps = 60
+    dt = 1 / fps
 
-	space = pymunk.Space()
-	space.gravity = (0, 980)
+    space = pymunk.Space()
+    space.gravity = (0, 980)
 
-	ball = create_ball(space, 30, 10)
+    ball = create_ball(space, 30, 10)
+    create_boundaries(space, width, height)
 
-	draw_options = pymunk.pygame_util.DrawOptions(window)
+    draw_options = pymunk.pygame_util.DrawOptions(window)
 
-	while run:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				run = False
-				break
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
 
+        draw(space, window, draw_options)
+        space.step(dt)
+        clock.tick(fps)
 
-		draw(space, window, draw_options)
-		space.step(dt)
-		clock.tick(fps)
+    pygame.quit()
 
-	pygame.quit()
 
 if __name__ == '__main__':
-
-	run(window, WIDTH, HEIGHT)
-
+    run(window, WIDTH, HEIGHT)

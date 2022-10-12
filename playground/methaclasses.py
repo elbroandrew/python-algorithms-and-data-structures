8cd8924441f8e4d493e0f71d6c0e76cfa6db7f17
andrew <elbroandrew@gmail.com>
2022-10-12 22:30:48 +1100

    metaclass with class

--- playground/methaclasses.py
+++ playground/methaclasses.py
@@ -4,12 +4,18 @@ type('MyClassName', (BaseClass1, BaseClass2,...), {dict of attributes})
 """
 
 
-def create_class_point(name, bases, attrs):
-    attrs.update({'MAX_VALUE': 100, 'MIN_VALUE': 0})
-    return type(name, bases, attrs)
+# def create_class_point(name, bases, attrs):
+#     attrs.update({'MAX_VALUE': 100, 'MIN_VALUE': 0})
+#     return type(name, bases, attrs)
+
+class Meta(type):
+    def __init__(cls, name, bases, attrs):
+        super().__init__(name, bases, attrs)
+        cls.MAX_VALUE = 100
 
 
-class Point(metaclass=create_class_point):
+
+class Point(metaclass=Meta):
     def get_coords(self):
         return (0, 0)
 

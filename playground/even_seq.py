628bd672283e5258fedd81799349f2f136480108
elbroandrew <elbroandrew@gmail.com>
2022-11-24 11:54:58 +1000

    invertor

--- playground/even_seq.py
+++ playground/even_seq.py
@@ -7,11 +7,15 @@ for 10 returns => 2, 4, 6, 8, 10
 
 
 def invert(func: Callable):
-    def wrapper(*args):
-        res: list = func(*args)
+    def wrapper(x: int):
+        res: list = func(x)
         try:
             if isinstance(res, list):
-                map(abs, res)
+                if x <= 0:
+                    res = [*map(abs, res)]
+                else:
+                    res = [-x for x in res]
+
         except RuntimeError:
             print("RESULT IS NOT LIST.")
         return res
@@ -24,16 +28,15 @@ def foo(seq: int = 10) -> list:
     if seq < 0:
         step = -1
 
-    return [x for x in range(0, seq, step) if abs(x) % 2 == 0]
+    return [x for x in range(0, seq, step) if abs(x) % 2 == 0 and x != 0]
 
 
 def test() -> None:
     try:
-        #assert foo(-10) == [0, -2, -4, -6, -8]
-        assert foo(10) == [0, 2, 4, 6, 8]
+        assert foo(-10) == [2, 4, 6, 8]
+        assert foo(10) == [-2, -4, -6, -8]
         print("TESTS PASSED.")
     except AssertionError:
-        print(foo(10))
         print("!! TESTS FAILED.")
 
 

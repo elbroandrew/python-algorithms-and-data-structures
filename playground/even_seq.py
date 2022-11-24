bd37b5bf2b34aa59ebc3c35450a27968dff49ec4
elbroandrew <elbroandrew@gmail.com>
2022-11-24 11:33:49 +1000

    add tests

--- playground/even_seq.py
+++ playground/even_seq.py
@@ -4,13 +4,21 @@ for 10 returns => 2, 4, 6, 8, 10
 """
 
 
-def foo(seq: int = 10):
+def foo(seq: int = 10) -> list:
     step = 1
     if seq < 0:
         step = -1
 
-    print(*[x for x in range(0, seq, step) if x % 2 <= 0])
+    return [x for x in range(0, seq, step) if abs(x) % 2 == 0]
 
 
-foo(-10)
-foo(10)
+def test() -> None:
+    try:
+        assert foo(-10) == [0, -2, -4, -6, -8]
+        assert foo(10) == [0, 2,4,6,8]
+        print("TESTS PASSED.")
+    except AssertionError:
+        print("!! TESTS FAILED.")
+
+
+test()

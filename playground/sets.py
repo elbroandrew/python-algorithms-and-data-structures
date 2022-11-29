86eb4e475de808fa0302e60c64a9ae3455a22583
elbroandrew <elbroandrew@gmail.com>
2022-11-29 10:39:29 +1000

    set cannot contain mutable types

--- playground/sets.py
+++ playground/sets.py
@@ -16,3 +16,14 @@ print(my_set)
 my_list = [1,1,1,1,2,3,4,5,5,5,5,5,5,5]
 another_set = set(my_list)
 print(another_set)
+
+"""
+Сет не может содержать мутабельные типы данных.
+"""
+a = 5 # immutable
+b = "test"  # immutable
+c = [1,2,3] # mutable or UNhashable
+
+# s = set([a, b, c]) # error for 'c' ---> TypeError: unhashable type: 'list'
+# print(s)
+#print(hash(c)) # same error

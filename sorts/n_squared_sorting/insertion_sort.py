f8e773d5efb4201b5f04a97c242788c544df9c70
Andrew <elbroandrew@gmail.com>
2023-05-07 22:05:17 +1000

    json indentation

--- sorts/n_squared_sorting/insertion_sort.py
+++ sorts/n_squared_sorting/insertion_sort.py
@@ -4,7 +4,7 @@ arr = [3, 5, 12, 4, 7, 8, 2, 1, 9, 0]
 for i in range(1, len(arr)):
     cur = arr[i]
     j = i - 1
-    while(j >= 0 and arr[j] > cur):
+    while j >= 0 and arr[j] > cur:
         arr[j + 1] = arr[j]
         j -= 1
     arr[j + 1] = cur

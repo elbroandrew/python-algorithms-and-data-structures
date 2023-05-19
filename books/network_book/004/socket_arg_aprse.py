941547a4227994296ef88edbf2b5a1e3a3c8d3f7
Andrew <elbroandrew@gmail.com>
2023-05-19 10:03:30 +1000

    add all error handlers for arg parsing script

--- books/network_book/004/socket_arg_aprse.py
+++ books/network_book/004/socket_arg_aprse.py
@@ -9,7 +9,7 @@ This script shows how to handle socket errors.
 logger = logging.getLogger("main")
 logger.setLevel(logging.DEBUG)
 
-#FIXME: LOG_DIR path may not be correct.
+
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
 
@@ -34,9 +34,47 @@ def main():
     port = given_args.port
     filename = given_args.file
 
-    # Create a socket -- First try-except block
+    # Create a socket -- first try-except block
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error as e:
         print("Error creating socket: %s" % e)
-        logger.error("Error creating socket: %s" % e, exc_info=True) # exc_info=True means exception text will also be added into the log file.
+        logger.error("Error creating socket: %s" % e, exc_info=True)
+        sys.exit(1)
+    # Connect to given host/port -- second try/except block
+    try:
+        s.connect((host, port))
+    except socket.gaierror as e:
+        print("Address-related error connecting to server: %s" % e)
+        logger.error("Address-related error connecting to server: %s" % e, exc_info=True)
+        sys.exit(1)
+    except socket.error as e:
+        print("Connection error: %s" % e)
+        logger.error("Connection error: %s" % e, exc_info=True)
+        sys.exit(1)
+    # Sending data -- third try/except block
+    try:
+        msg = "GET %s HTTP/1.0\r\n\r\n" % filename
+        s.sendall(msg.encode('utf-8'))
+    except socket.error as e:
+        print("Error sending data: %s" % e)
+        logger.error("Error sending data: %s" % e, exc_info=True)
+        sys.exit(1)
+    while 1:
+        # waiting to receive data from host -- fourth block
+        try:
+            buf = s.recv(2048)
+        except socket.error as e:
+            print("Error receiving data: %s" % e)
+            logger.error("Error receiving data: %s" % e, exc_info=True)
+            sys.exit(1)
+        if not len(buf):
+            break
+        # write the received data
+        sys.stdout.write(buf.decode('utf-8'))
+
+if __name__ == '__main__':
+    main()
+
+
+

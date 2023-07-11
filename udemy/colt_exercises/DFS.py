3292baa9eb9224010670356ea38f0579e96754d9
Andrew <elbroandrew@gmail.com>
2023-07-11 15:55:24 +1000

    DFS POST order solution

--- udemy/colt_exercises/DFS.py
+++ udemy/colt_exercises/DFS.py
@@ -1,6 +1,5 @@
 from binary_search_tree import *
 
-"""PRE-ORDER SOLUTION"""
 
 t = BinarySearchTree()
 t.insert(10)
@@ -9,6 +8,9 @@ t.insert(3)
 t.insert(8)
 t.insert(15)
 t.insert(20)
+
+
+"""PRE-ORDER SOLUTION"""
 
 def dfs_pre_order(b: BinarySearchTree):
     data = []
@@ -21,4 +23,22 @@ def dfs_pre_order(b: BinarySearchTree):
     traverse(b.root)
     return data
 
-print(dfs_pre_order(t))
+print("PRE order solution: %s" % dfs_pre_order(t) )  # PRE order solution: [10, 6, 3, 8, 15, 20]
+
+"""POST-ORDER SOLUTION"""
+
+def dfs_post_order(b:BinarySearchTree):
+    visited = []
+
+    def traverse(node: Node):
+        if node.left:
+            traverse(node.left)
+        if node.right:
+            traverse(node.right)
+        visited.append(node.value)
+
+    traverse(b.root)
+
+    return visited
+
+print("POST order solution: %s" % dfs_post_order(t) )  # POST order solution: [3, 8, 6, 20, 15, 10]

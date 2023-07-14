8c63a8428e64d3d6880725d80d7b6c7a1c73b960
Andrew <elbroandrew@gmail.com>
2023-07-14 10:26:45 +1000

    dijkstra weighted graph: simple priority queue

--- playground/GRAPH/weighted_graph.py
+++ playground/GRAPH/weighted_graph.py
@@ -1,3 +1,22 @@
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.values = []
+
+    def sort(self):
+        self.values.sort(key=lambda x: x['priority'])  #  sort dicts inside the list by priority
+
+    def enqueue(self, val, priority):
+        self.values.append(dict(val=val, priority=priority))
+        self.sort()
+
+    def dequeue(self):
+        return self.values.pop(0)
+
+
+
+
 class WeightedGraph:
 
     def __init__(self):
@@ -13,3 +32,14 @@ class WeightedGraph:
         self.adj_list[vertex2].append(dict(node=vertex1, weight=weight))
 
 
+def main():
+    q = PriorityQueue()
+    q.enqueue("B", 3)
+    q.enqueue("C", 5)
+    q.enqueue("D", 2)
+    q.enqueue("Q", 20)
+    print(q.values)
+
+
+if __name__ == '__main__':
+    main()

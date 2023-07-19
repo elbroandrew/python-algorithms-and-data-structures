ba5cd2aa52cc127753b1f29d99bad4e23430363f
andrew <elbroandrew@gmail.com>
2023-07-19 16:23:22 +1100

    devops book: os path get last time file accessed

--- books/devops_book/c001/os_tutorial.py
+++ books/devops_book/c001/os_tutorial.py
@@ -2,6 +2,7 @@
 # because for each platform there is specific mehods in 'os' module
 # such as: Windows os.O_TEXT, GNU/Linux os.CLOEXEC
 import os
+import time
 
 print(os.stat("text"))
 p = os.stat("text")
@@ -16,3 +17,12 @@ print(os.path.expanduser("~"))
 print(os.environ)
 print(os.path.expandvars("$HOME/.ssh"))  # короче берет из окружения переменную и подставляет ее путь в виде $HOME или ${HOME}
 
+# get last access time and size
+children = os.listdir()
+for child in children:
+    child_path = os.path.join(os.getcwd(), child)
+    #print(child_path)
+    if os.path.isfile(child_path):
+        last_access_time = os.path.getatime(child_path)
+        size = os.path.getsize(child_path)
+        print("Path: %s, last time accessed: %s; size: %s;" % (child, time.ctime(last_access_time), size))

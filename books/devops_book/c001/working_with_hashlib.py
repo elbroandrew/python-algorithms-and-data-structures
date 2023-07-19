5eb04d83dee248ecbc4c05c123d0e3dce863e8fe
andrew <elbroandrew@gmail.com>
2023-07-19 15:15:34 +1100

    devops book: fernet encryption with key

--- books/devops_book/c001/working_with_hashlib.py
+++ books/devops_book/c001/working_with_hashlib.py
@@ -1,4 +1,5 @@
 import hashlib
+from cryptography.fernet import Fernet
 
 secret = "This is the secret password"
 bsecret = secret.encode()  # convert to binary string
@@ -7,3 +8,15 @@ m = hashlib.md5()
 m.update(bsecret)
 
 print(m.digest())
+
+# example with cryptography Fernet (AES algo)
+key = Fernet.generate_key()
+print(key)
+# encrypt the data using Fernet object (to store the key)
+f = Fernet(key)
+message = b'Hello World'
+encrypted = f.encrypt(message)
+print(encrypted)
+# decrypt
+print(f.decrypt(encrypted))
+

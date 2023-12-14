fb7d67f39a06eb4914b9f9b4d9643b363b9b1266
Andrew <elbroandrew@gmail.com>
2023-12-14 14:38:05 +1000

    glob remove files

---
+++ playground/remove_files_recursive_glob_win10.py
@@ -0,0 +1,22 @@
+import glob
+import os
+import fnmatch
+
+
+# remove unnecessary files
+
+path = r"\\?\C:\Users\user\Downloads\IBM Data Engineering Professional Certificate 2023-8\**"
+
+for file_name in glob.glob(pathname=path, recursive=True):
+    if os.path.isdir(file_name):
+        continue
+    
+    if not fnmatch.fnmatch(file_name, '*.mp4'):
+        try:
+
+            os.remove(file_name)
+            
+            print(f"{file_name} was deleted.") 
+            
+        except Exception as e:
+            print(e)

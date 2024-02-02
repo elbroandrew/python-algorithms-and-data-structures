d8994e8c19cf3a7acad6b23a5b03064acd013242
andrew <elbroandrew@gmail.com>
2024-02-02 13:09:32 +1000

    ordered dict move last elem to the beginning

--- playground/_collections.py
+++ playground/_collections.py
@@ -7,7 +7,7 @@ print(first)
 print(first.popitem(last=False))  # print first element
 
 d2 = OrderedDict({'a':1, 'b':2, 'c':3})
-d2.move_to_end('a') # OrderedDict([('b', 2), ('c', 3), ('a', 1)])
+d2.move_to_end('c', last=False) # OrderedDict([('c', 3), ('a', 1),('b', 2)])
 
 print(d2)
 

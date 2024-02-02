9ed6cd1fa4811866e977d03abaa959cfe641efbc
andrew <elbroandrew@gmail.com>
2024-02-02 13:05:25 +1000

    OrderedDict move element to end

---
+++ playground/_collections.py
@@ -0,0 +1,14 @@
+
+from collections import OrderedDict
+
+first = OrderedDict({1: 1, 2: 2})
+
+print(first)
+print(first.popitem(last=False))  # print first element
+
+d2 = OrderedDict({'a':1, 'b':2, 'c':3})
+d2.move_to_end('a') # OrderedDict([('b', 2), ('c', 3), ('a', 1)])
+
+print(d2)
+
+

deb77b11affb7c68f2de26dc09a886d85de2c8ad
andrew <elbroandrew@gmail.com>
2024-02-02 17:26:27 +1000

    create points from csv with namedtuple

--- playground/_collections.py
+++ playground/_collections.py
@@ -1,24 +1,16 @@
-from collections import deque
+from collections import namedtuple
+import csv
 
-# deque вставляет в начало очень быстро,  отличие от списка
+# Cat = namedtuple('Cat', "name age color")
+# tom = Cat('Tom', 4, 'black')
+# print(tom)
 
-q = deque(iterable=[1,2,3,4,5,6,7])
-q.appendleft(100)
-print(q)
+POINT = namedtuple("POINT", "x y")
 
-q2 = deque([1,2,3,4], maxlen=3)
-print(q2) # 2,3,4
-
-# use maxlen with file to print last 2 strings -- similar to unix "tail" command:
-
-with open("points.csv") as file:
-    a_deque = deque(file, maxlen=2)
-
-for line in a_deque:
-    print(line.rstrip())  # remove '\n'
-
-# дека - потокобезопасна, можно использовать в многопоточных приложениях, например pub/sub паттерн.
-
+with open('points.csv') as file:
+    for line in csv.reader(file):
+        point = POINT._make(line)
+        print(point)
 
 
 

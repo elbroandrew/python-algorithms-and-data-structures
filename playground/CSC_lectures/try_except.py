3d2bd8814e0f5621cc36cca25616aabb8fdab5d0
Andrew <elbroandrew@gmail.com>
2024-02-15 16:38:29 +1000

    правильн ообработать открытие текста

--- playground/CSC_lectures/try_except.py
+++ playground/CSC_lectures/try_except.py
@@ -1,9 +1,10 @@
-import sys
-import traceback
 
-try:
-    import mymodule
-except Exception as e:
-    print(e.with_traceback(e.__traceback__))
-    import sys
-    traceback.print_tb(e.__traceback__)
+
+with open("mytext.txt") as f:
+    text = f.read()
+    process(text)  # надо закрыть файл как можно скорее.
+    
+    
+with open("mytext.txt") as f:
+    text = f.read()
+process(text)  # поэтому лучше сделать вот так, тем более в питоне скопов не с with (или не только с with?)

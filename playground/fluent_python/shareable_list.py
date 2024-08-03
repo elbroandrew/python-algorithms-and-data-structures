44252f2976a17503021611d39d174305666fa89a
Andrew <elbroandrew@gmail.com>
2024-08-03 18:55:33 +1000

    process shareable list

---
+++ playground/fluent_python/shareable_list.py
@@ -0,0 +1,35 @@
+from multiprocessing.shared_memory import ShareableList
+from multiprocessing import Process, freeze_support
+
+'''
+ShareableList is used for data transfer between processes.
+Shared between processes means that changes made to the list in 
+one process will be visible and accessible in another process.
+'''
+
+sl = ShareableList([1,2,3,4])
+
+# create a task
+
+def task(sl: ShareableList):
+    print(sl)
+    for i in range(len(sl)):
+        sl[i] = sl[i] ** 2
+
+
+
+if __name__ == "__main__":
+    freeze_support()  # needed for Windows
+    
+    # create a process to execute the task
+    p1 = Process(target=task, args=(sl,))
+
+    p1.start()
+    p1.join()
+
+    print(sl)
+
+    # close the shared memory
+    sl.shm.close()
+    # release the shared memory
+    sl.shm.unlink()

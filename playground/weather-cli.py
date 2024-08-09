3ad5f7b6557f570ea41bbe4d9105914b73e54553
Andrew <elbroandrew@gmail.com>
2024-08-09 16:07:21 +1000

    weather cli

---
+++ playground/weather-cli.py
@@ -0,0 +1,24 @@
+import click
+import httpx
+from scrapy.selector import Selector
+import asyncio
+
+@click.command()
+@click.option('--city', default="komsomolsk-na-amure", help="City to get weather for")
+def weather(city):
+  
+  url = f'https://pogoda.mail.ru/prognoz/{city}/'
+  with httpx.Client() as client:
+    resp = client.get(url, timeout=None)
+    resp.raise_for_status()
+    
+  selector = Selector(text=resp.text)
+  t = selector.xpath('//div[@class="information__content__temperature"]/text()').getall()[1].strip()
+  s = selector.xpath('//div[@class="information__content__temperature"]/span/@title').extract()[0]
+  
+  print(f"City: {city}, T°: {t}, \nsource: pogoda.mail.ru")
+  print(s)
+  
+
+if __name__ == '__main__':
+  weather()

18f5c7e8d4e72f94839382df1969ba48da102aef
Andrew <elbroandrew@gmail.com>
2025-05-17 19:16:58 +1000

    add tests

--- leetcode/greatest_String_divider.py
+++ leetcode/greatest_String_divider.py
@@ -1,8 +1,9 @@
+import unittest
 
 def find_gcd_substring(s1: str, s2: str):
     # Find greatest string divider
     if not s1+s2 == s2+s1:
-        return "gyuhk"
+        return ""
     
     a = len(s1)
     b = len(s2)
@@ -13,22 +14,26 @@ def find_gcd_substring(s1: str, s2: str):
         a = temp
     
     gcd = a
-    print(gcd)
        
     return "".join(s1[:gcd])
     
-print(find_gcd_substring("ABABABAB", "ABAB"))
 
+class TestGCDSubstring(unittest.TestCase):
+    
+
+    def test_gcd(self):
+        test_cases = [
+            ("ABABABAB", "ABAB",  "ABAB"),
+            ("ABCABC", "ABC", "ABC"),
+            ("ABABAB", "ABAB", "AB"),
+            ("LEET", "CODE", "")
+        ]
+    
+        for s1, s2, answer in test_cases:
+            with self.subTest(s1=s1, s2=s2):  # allows each test case to run independently, so if one fails, the others still run and you can see which specific cases failed.
+                self.assertEqual(find_gcd_substring(s1, s2), answer)
 
 
 
 if __name__ == "__main__":
-    
-
-'''
-
-"ABABABAB" "ABAB"  -> ABAB
-"ABCABC" "ABC"  -> ABC
-"ABABAB" "ABAB" -> AB
-
-'''
+    unittest.main()
